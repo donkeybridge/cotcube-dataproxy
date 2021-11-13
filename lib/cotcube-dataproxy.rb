05ac310e39fb1ce4d449c9cfccb101ee1a5e0ae7
Benjamin L. Tischendorf <donkeybridge@jtown.eu>
2021-11-13 21:15:08 +0000

    central startup file, containing bundler, requires and constants

---
+++ lib/cotcube-dataproxy.rb
@@ -0,0 +1,36 @@
+#!/usr/bin/env ruby
+
+require 'bundler'
+Bundler.require
+require_relative './cotcube-dataproxy/3rd_clients'
+require_relative './cotcube-dataproxy/init'
+require_relative './cotcube-dataproxy/client_response'
+require_relative './cotcube-dataproxy/subscribe_messages'
+require_relative './cotcube-dataproxy/commserver'
+require_relative './cotcube-dataproxy/gc'
+
+SECRETS_DEFAULT = { 
+  'dataproxy_mq_proto'    => 'http',
+  'dataproxy_mq_user'     => 'guest', 
+  'dataproxy_mq_password' => 'guest',
+  'dataproxy_mq_host'     => 'localhost',
+  'dataproxy_mq_port'     => '15672',
+  'dataproxy_mq_vhost'    => '%2F'
+}
+
+# Load a yaml file containing actual parameter and merge those with current
+# TODO use better config file location
+SECRETS = SECRETS_DEFAULT.merge( -> {YAML.load(File.read("#{__FILE__.split('/')[..-2].join('/')}/../secrets.yml")) rescue {} }.call)
+
+CANCEL_TYPES = {
+  ticks: :CancelMarketData,
+  depth: :CancelMarketDepth,
+  realtimebars: :CancelRealTimeBars
+}
+
+REQUEST_TYPES = { 
+  ticks: :RequestMarketData,
+  depth: :RequestMarketDepth,
+  realtimebars: :RequestRealTimeBars
+}
+
