0564c8d03ede328a01f0f1fc86d1bf50ad5016b2
Benjamin L. Tischendorf <donkeybridge@jtown.eu>
2021-11-13 21:27:35 +0000

    client_response.rb, containing client_success and client_fail

---
+++ lib/cotcube-dataproxy/client_response.rb
@@ -0,0 +1,46 @@
+module Cotcube
+  class DataProxy
+
+    def client_success(request, id: nil, to: nil, exchange: :replies_exch, &block)
+      client_response( request, id: nil, to: nil, err: 0, exchange: exchange, &block)
+    end
+
+    def client_fail(request,    id: nil, to: nil, exchange: :replies_exch, &block)
+      client_response(request,  id: nil, to: nil, err: 1, exchange: exchange, &block)
+    end
+
+    private
+
+    def client_response(request, id: nil, to: nil, err:, exchange: :replies_exch)
+      __id__ = id.presence || request.delete(:__id__)
+      __to__ = to.presence || request.delete(:__to__)
+      msg = yield
+      case msg
+      when String
+        response = { error: err, msg: msg }
+      when Hash
+        response = { error: err }
+        msg.each { |k, v| response[k] = v }
+      when Array
+        response = { error: err, result: msg }
+      else
+        response = { error: 1, msg: "Processing failed for '#{msg.inspect}' after '#{request}'." }
+      end
+      if response[:error] == 1
+        log "CLIENT #{id} FAILIURE:      #{response.inspect}.".colorize(:light_red)
+      elsif response[:result].is_a?(Array)
+        log "CLIENT #{id} SUCCESS:       sent #{response[:result].size} datasets."
+      else
+        log "CLIENT #{id} SUCCESS:       #{response.to_s.scan(/.{1,120}/).join(' '*30 + "\n")}"
+      end
+      mq[exchange].publish(
+        response.to_json,
+        content_type:    'application/json',
+        priority: 7,
+        correlation_id:  __id__,
+        routing_key:     __to__,
+        reply_to:        __id__
+      )
+    end
+  end
+end
