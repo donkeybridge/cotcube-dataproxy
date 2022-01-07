17845f20ec2c4cd4219fd41f3f8681b70c60428f
Benjamin L. Tischendorf <donkeybridge@jtown.eu>
2022-01-07 22:02:28 +0000

    changing monit check to 'real dataproxy' checks, TODO: create according tws and cccache checks

--- bin/dp_status.rb
+++ bin/dp_status.rb
@@ -16,21 +16,21 @@ finish.call if now.wday == 6
 # dont check friday past 16
 finish.call if now.wday == 5 and now.hour > 16
 
-breakfast = JSON.parse(`/usr/local/bin/cccache breakfast 55555557`, symbolize_names: true)
-contract  = 'ESH22' #breakfast[:payload].first[:contract]
+# breakfast = JSON.parse(`/usr/local/bin/cccache breakfast 55555557`, symbolize_names: true)
+# contract  = 'ESH22' #breakfast[:payload].first[:contract]
 result    = [] 
 begin 
   Timeout.timeout(10) do 
-    result = Cotcube::Helpers::DataClient.new.get_historical(contract: contract, interval: :min30, duration: '2_D' )
+    # result = Cotcube::Helpers::DataClient.new.get_historical(contract: contract, interval: :min30, duration: '2_D' )
+    result = Cotcube::Helpers::DataClient.new.send_command({ command: 'ping', monitoring: true })
   end
 rescue Timeout::Error
   finish.call('Timeout: No response from dataproxy', 1)
 end
 
 finish.call('Could not parse response from dataproxy', 1)  unless (result = JSON.parse(result, symbolize_names: true) rescue false)
-
-finish.call('invalid result, no base contained', 1) if result[:base].nil?
-finish.call('invalid result, base contained to few records', 1) if result[:base].size < 10
+finish.call('invalid result, no error value contained', 1) if result[:error].nil?
+finish.call("invalid result, base returned error: '#{result[:message]}'" , 1) unless  result[:error].zero?
 finish.call('OK',0) 
 
 
